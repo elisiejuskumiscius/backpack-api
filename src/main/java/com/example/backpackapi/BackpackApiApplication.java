971ed0a1ea0ae6f591baa6f507e750054d83a658
elisiejuskumiscius <ekumiscius@gmail.com>
2024-02-29 21:22:06 +0200

    Project innit

---
+++ src/main/java/com/example/backpackapi/BackpackApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.backpackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackpackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backpackapi/BackpackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backpackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
